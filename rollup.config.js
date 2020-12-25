81b68b303c6b57da9ea1224e827fd0ddcb6649ed
avi12 <avi6106@gmail.com>
2020-12-25 21:28:43 +0200

    Switching from a build script to Rollup + plugins

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import {
+  chromeExtension,
+  simpleReloader
+} from "rollup-plugin-chrome-extension";
+import { terser } from "rollup-plugin-terser";
+import zip from "rollup-plugin-zip";
+import { emptyDir } from "rollup-plugin-empty-dir";
+
+const isProduction = !process.env.ROLLUP_WATCH;
+export default {
+  input: "src/manifest.json",
+  output: {
+    dir: "dist",
+    format: "esm"
+  },
+  plugins: [
+    chromeExtension(),
+    isProduction && simpleReloader(),
+    emptyDir(),
+    isProduction && terser(),
+    isProduction && zip({ dir: "dist_packed" })
+  ]
+};
