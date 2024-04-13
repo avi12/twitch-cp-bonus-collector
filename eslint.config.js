41f28fed65738c42686ca07a4947cf7bb1a1e188
avi12 <avi6106@gmail.com>
2024-04-13 03:00:49 +0300

    Completed migration to ESLint's flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,77 +1,33 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended
-});
+import eslint from "@eslint/js";
+import avi12 from "eslint-config-avi12";
+import svelteEslint from "eslint-plugin-svelte";
+import globals from "globals";
+import svelteParser from "svelte-eslint-parser";
+import tsEslint from "typescript-eslint";
 
 export default [
-  ...compat.config({
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      lib: ["dom", "esnext"]
-    },
-    overrides: [
-      {
-        files: ["*.svelte"],
-        parser: "svelte-eslint-parser",
-        parserOptions: {
-          parser: "@typescript-eslint/parser"
-        }
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...svelteEslint.configs["flat/recommended"],
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsEslint.parser
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        chrome: true
       }
-    ],
-    rules: {
-      "arrow-parens": ["warn", "as-needed"],
-      "prefer-const": "warn",
-      "no-async-promise-executor": "off",
-      semi: ["warn", "always"],
-      quotes: [
-        "warn",
-        "double",
-        {
-          allowTemplateLiterals: true
-        }
-      ],
-      indent: [
-        "warn",
-        2,
-        {
-          SwitchCase: 1
-        }
-      ],
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "object-curly-spacing": ["warn", "always"],
-      "quote-props": ["warn", "as-needed"],
-      "comma-dangle": ["warn", "never"],
-      curly: ["warn", "all"],
-      "array-element-newline": ["warn", "consistent"],
-      "import/order": ["warn", { groups: ["external", "internal"], alphabetize: { order: "asc" } }],
-      "object-property-newline": [
-        "warn",
-        {
-          allowMultiplePropertiesPerLine: true
-        }
-      ]
-    },
-    env: {
-      browser: true,
-      es2024: true,
-      node: true,
-      webextensions: true
-    },
-    globals: {
-      chrome: true
-    },
-    plugins: ["svelte", "@typescript-eslint", "import"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended"]
-  })
+    }
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsEslint.parser
+    }
+  },
+  ...avi12
 ];
